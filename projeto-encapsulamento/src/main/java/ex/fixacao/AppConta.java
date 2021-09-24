2ee7cea78fe447b9904e6ab73f4e8b5c4ff95b35
pettersoon <79462873+pettersoon@users.noreply.github.com>
2021-09-24 14:24:21 -0300

    encapsulamento-contaBancaria

---
+++ projeto-encapsulamento/src/main/java/ex/fixacao/AppConta.java
@@ -0,0 +1,14 @@
+package ex.fixacao;
+
+public class AppConta {
+    
+    public static void main(String[] args) {
+        
+        ContaBancaria cliente1 = new ContaBancaria("Petterson", 0.0);
+        
+        
+        cliente1.realizarOperacao();
+        
+        
+    }
+}
